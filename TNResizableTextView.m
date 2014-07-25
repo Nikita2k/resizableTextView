2ed52d24633f0bedfd6c40aae26ace2e7b165653
Nikita2k <654070@gmail.com>
2014-07-25 13:46:29 +0600

    Create TNResizableTextView.m

---
+++ TNResizableTextView.m
@@ -0,0 +1,24 @@
+//
+//  TNResizableTextView.m
+//
+//  Created by Nikita Took on 25.07.14.
+//
+
+#import "TNResizableTextView.h"
+
+@implementation ResizableTextView
+- (void) updateConstraints {
+    // calculate contentSize manually (ios7 doesn't calculate it before viewDidAppear, and we'll get here before)
+    CGSize contentSize = [self sizeThatFits:CGSizeMake(self.frame.size.width, FLT_MAX)];
+    
+    // set the height constraint to change textView height
+    [self.constraints enumerateObjectsUsingBlock:^(NSLayoutConstraint *constraint, NSUInteger idx, BOOL *stop) {
+        if (constraint.firstAttribute == NSLayoutAttributeHeight) {
+            constraint.constant = contentSize.height;
+            *stop = YES;
+        }
+    }];
+    
+    [super updateConstraints];
+}
+@end
